1f5d7f7f5439b0a2913ed8be225896e85bd84bdd
Aprianto Wiwarsono <awiwarsono@gmail.com>
2018-01-19 14:43:35 +1300

    Complete palindrome

--- src/recursion.js
+++ src/recursion.js
@@ -134,6 +134,12 @@ var reverse = function(string) {
 
 // 10. Write a function that determines if a string is a palindrome.
 var palindrome = function(string) {
+  if (string.length <= 1) {
+    return true;
+  } else if (string.charAt(0).toUpperCase() === string.charAt(string.length - 1).toUpperCase()) {
+    return palindrome(string.slice(1, string.length - 1))
+  }
+  return false;
 };
 
 // 11. Write a function that returns the remainder of x divided by y without using the
